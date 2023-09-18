f99a171d4fc39074bb855be6ea06cc9ae5843d13
Revolution <tavinho.daniel@gmail.com>
2023-09-18 19:58:40 -0300

    feat: update application structure and implement MVVM pattern

--- ProcessMonitor/App.xaml.cs
+++ ProcessMonitor/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace process_monitor
+namespace ProcessMonitor
 {
     /// <summary>
     /// Interaction logic for App.xaml
---
+++ ProcessMonitor/Views/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf.Ui;
+using Wpf.Ui.Controls;
+
+namespace ProcessMonitor.Views;
+
+public partial class MainWindow : INavigationWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    public INavigationView GetNavigation()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Navigate(Type pageType)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetServiceProvider(IServiceProvider serviceProvider)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetPageService(IPageService pageService)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ShowWindow()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CloseWindow()
+    {
+        throw new NotImplementedException();
+    }
+}
