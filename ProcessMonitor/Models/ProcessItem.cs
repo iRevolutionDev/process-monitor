b221d49b293c3e62e6d85dcab387c6cedc11c40d
Revolution <tavinho.daniel@gmail.com>
2023-09-19 21:57:34 -0300

    refactor: ProcessesViewModel and update ProcessesPage

---
+++ ProcessMonitor/Models/ProcessItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace ProcessMonitor.Models;
+
+public class ProcessItem
+{
+    public ProcessItem(Process process)
+    {
+        Process = process;
+    }
+
+    private Process Process { get; }
+
+    public string Name => Process.ProcessName;
+    public int Id => Process.Id;
+}
--- ProcessMonitor/ViewModels/ProcessesViewModel.cs
+++ ProcessMonitor/ViewModels/ProcessesViewModel.cs
@@ -1,16 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
+using ProcessMonitor.Models;
 using Wpf.Ui.Controls;
 
 namespace ProcessMonitor.ViewModels;
 
 public partial class ProcessesViewModel : ObservableObject, INavigationAware
 {
+    [ObservableProperty] private bool _handles;
+    [ObservableProperty] private bool _id;
     private bool _isInitialized;
 
-    [ObservableProperty] private List<Process> _processes = new();
+    // Columns
+    [ObservableProperty] private bool _name;
+
+    [ObservableProperty] private ObservableCollection<ProcessItem> _processes = new();
+    [ObservableProperty] private ProcessItem? _selectedProcess;
+    [ObservableProperty] private bool _threads;
 
     public void OnNavigatedTo()
     {
@@ -24,12 +32,21 @@ public partial class ProcessesViewModel : ObservableObject, INavigationAware
 
     private void UpdateProcesses()
     {
-        Processes = Process.GetProcesses().ToList();
+        var processes = Process.GetProcesses().OrderBy(p => p.ProcessName).ToList();
+
+        Processes.Clear();
+
+        foreach (var process in processes)
+            Processes.Add(new ProcessItem(process));
     }
 
     private void InitializeViewModel()
     {
         UpdateProcesses();
+
+        Name = true;
+        Id = true;
+        Threads = true;
 
         _isInitialized = true;
     }
